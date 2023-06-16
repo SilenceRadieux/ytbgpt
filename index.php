0e6d0786e43e6110a688affe5260717f3241ec69
SilenceRadieux <quentin.graff@hotmail.fr>
2023-06-16 16:27:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <button id="btn-style">X</button>
+    <button id="btn-years">X</button>
+    <button id="btn-knowledge">X</button>
+    <button id="btn-mood">X</button>
+    <button id="btn-languages">X</button>
+    <button id="btn-hobbies">X</button>
+</html>
