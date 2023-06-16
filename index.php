4b0c7a64cbf51c81e15b9cf0e2f9305eb1b41ceb
SilenceRadieux <quentin.graff@hotmail.fr>
2023-06-16 16:41:28 +0200

    folders

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<html>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>ytbgpt</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js" defer></script>
+</head>
+<body>
+    <header>
+        <h1>ytbgpt</h1>
+    </header>
     <button id="btn-style">X</button>
     <button id="btn-years">X</button>
     <button id="btn-knowledge">X</button>
     <button id="btn-mood">X</button>
     <button id="btn-languages">X</button>
     <button id="btn-hobbies">X</button>
+</body>
 </html>
